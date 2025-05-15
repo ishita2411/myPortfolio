d33656c3737783c949eb20a4d191134de4fdbd67
Ishita Kundaliya <kundaliyaishita@gmail.com>
2025-05-14 20:29:38 -0500

    created new project

---
+++ my_portfolio/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: "Ishita Kundaliya",
+  description: "created by Ishita Kundaliya",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
